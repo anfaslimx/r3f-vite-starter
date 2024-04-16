1d94c1ca68ed69bf4d3c2b389e01de5568209752
anfa slim <marcellkey@gmail.com>
2024-04-15 20:51:39 -0500

    committed

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -120,19 +120,48 @@ const languages = [
 const SkillsSection = () => {
   return (
     <Section>
-      <div>
+      <motion.div whileInView={"visible"}>
         <h2 className="text-5xl font-bold">Skills</h2>
         <div className=" mt-8 space-y-4">
           {skills.map((skill, index) => (
             <div className="w-64" key={index}>
-              <h3 className="text-x1 font-bold text-gray-800">{skill.title}</h3>
+              <motion.h3
+                className="text-xl font-bold text-gray-800"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
               <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-                <div
-                className="h-full bg-indigo-500 rounded-full"
-                style={{ width: `${skill.level}%`}}
+                <motion.div
+                  className="h-full bg-indigo-500 rounded-full "
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
                 />
               </div>
-            </div>    
+            </div>
           ))}
         </div>
         <div>
@@ -140,18 +169,47 @@ const SkillsSection = () => {
           <div className=" mt-8 space-y-4">
             {languages.map((lng, index) => (
               <div className="w-64" key={index}>
-                <h3 className="text-xl font-bold text-gray-800">{lng.title}</h3>
+                <motion.h3
+                  className="text-xl font-bold text-gray-800"
+                  initial={{
+                    opacity: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 2 + index * 0.2,
+                      },
+                    },
+                  }}
+                >
+                  {lng.title}
+                </motion.h3>
                 <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-                  <div
-                    className="h-full bg-indigo-500 rounded-full"
-                    style={{ width: `${lng.level}%`}}
+                  <motion.div
+                    className="h-full bg-indigo-500 rounded-full "
+                    style={{ width: `${lng.level}%` }}
+                    initial={{
+                      scaleX: 0,
+                      originX: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        scaleX: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 2 + index * 0.2,
+                        },
+                      },
+                    }}
                   />
                 </div>
               </div>
             ))}
           </div>
         </div>
-      </div>
+      </motion.div>
     </Section>
   );
 };
