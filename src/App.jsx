4ae1398591e31f3631d9f8bbdbec52e763ab77f8
anfa slim <marcellkey@gmail.com>
2024-04-16 05:07:20 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { Canvas } from "@react-three/fiber";
-import { Experience } from "./components/Experience";
 import { Scroll, ScrollControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { MotionConfig } from "framer-motion";
+import { Leva } from "leva";
+import { useEffect, useState } from "react";
+import { Cursor } from "./components/Cursor";
+import { Experience } from "./components/Experience";
 import { Interface } from "./components/Interface";
-import { useState } from "react";
+import { Menu } from "./components/Menu";
 import { ScrollManager } from "./components/ScrollManager";
-import { Menu } from "./components/Menu"
+import { framerMotionConfig } from "./config";
 
 function App() {
   const [section, setSection] = useState(0);
@@ -15,22 +19,32 @@ function App() {
   }, [section]);
 
   return (
-    <> 
-    <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
-      <color attach="background" args={["#e6e7ff"]} />
-      <ScrollControls pages={4} damping={0.1}>
-        <ScrollManager section={section} onSectionChange={setSection}/>
-       <Experience section={section} />
-       <Scroll html>
-          <Interface />
-       </Scroll>
-      </ScrollControls>
-    </Canvas>
-    <Menu 
-      onSectionChange={setSection}
-      menuOpen={menuOpened}
-      setMenuOpened={setMenuOpened}
-   />
+    <>
+      <MotionConfig
+        transition={{
+          ...framerMotionConfig,
+        }}
+      >
+        <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+          <color attach="background" args={["#e6e7ff"]} />
+          <ScrollControls pages={4} damping={0.1}>
+            <ScrollManager section={section} onSectionChange={setSection} />
+            <Scroll>
+              <Experience section={section} menuOpened={menuOpened} />
+            </Scroll>
+            <Scroll html>
+              <Interface />
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+        <Menu
+          onSectionChange={setSection}
+          menuOpened={menuOpened}
+          setMenuOpened={setMenuOpened}
+        />
+        <Cursor />
+      </MotionConfig>
+      <Leva hidden />
     </>
   );
 }
