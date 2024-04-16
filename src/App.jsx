6d0cd80fbd07e9c7a370436279734dbe38d428e6
anfa slim <marcellkey@gmail.com>
2024-04-16 05:37:27 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Experience } from "./components/Experience";
 import { Interface } from "./components/Interface";
 import { Menu } from "./components/Menu";
 import { ScrollManager } from "./components/ScrollManager";
-import { framerMotionConfig } from "./config";
+import { framerMotionConfig } from "../config";
 
 function App() {
   const [section, setSection] = useState(0);
