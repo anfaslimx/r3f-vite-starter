96e148dd618362437efdbb2108988d20ed21e522
anfa slim <marcellkey@gmail.com>
2024-04-16 03:28:33 -0500

    committ

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -1,28 +1,28 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Section = (props) => {
-  const {children} = props;
+  const { children } = props;
 
   return (
-   <motion.section 
-     className={`
-    h-screen w-screen p-8 max-w-screen-2xl mx-auto
-    flex flex-col items-start justify-center
+    <motion.section
+      className={`
+  h-screen w-screen p-8 max-w-screen-2xl mx-auto
+  flex flex-col items-start justify-center
   `}
-  initial={{
-    opacity: 0,
-    y: 50,
-  }}
-  whileInView={{
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 1,
-      delay: 0.6,
-    }
-  }}
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.6,
+        },
+      }}
     >
-     {children}
+      {children}
     </motion.section>
   );
 };
@@ -41,46 +41,47 @@ export const Interface = () => {
 };
 
 const AboutSection = () => {
-  Return (
-  <Section>
-     <h1 className="text-6xl font-extrabold leading-snug">
+  return (
+    <Section>
+      <h1 className="text-6xl font-extrabold leading-snug">
         Hi, I'm
         <br />
-      <span className="bg-white px-1 italic">ANFASLIM</span>
+        <span className="bg-white px-1 italic">Anfa Slim</span>
       </h1>
-      <motion.p className="text-lg text-gray-600 mt-4"
-      initial={{
-        opacity: 0,
-        y: 25,
-      }}
-      whileInView={{
-       opacity: 1,
-       y: 0, 
-      }}
-      transition={{
-        duration: 1,
-        delay: 1.5,
-      }}
+      <motion.p
+        className="text-lg text-gray-600 mt-4"
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.5,
+        }}
       >
-        an experimental visual artist
+        Im a visual artist and designer 
         <br />
-        expanding my skill set
+        who loves to create beautiful things
       </motion.p>
       <motion.button
         className={`bg-indigo-600 text-white py-4 px-8 
       rounded-lg font-bold text-lg mt-16`}
-      initial={{
-        opacity: 0,
-        y: 25,
-      }}
-      whileInView={{
-       opacity: 1,
-       y: 0, 
-      }}
-      transition={{
-        duration: 1,
-        delay: 2,
-      }}
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 2,
+        }}
       >
         Contact me
       </motion.button>
@@ -90,32 +91,32 @@ const AboutSection = () => {
 
 const skills = [
   {
-    title: "Threejs / React Three ",
-    level: 30,
+    title: "Illustration",
+    level: 100,
   },
   {
-    title: "3D Animation",
+    title: "Animation",
+    level: 90,
+  },
+  {
+    title: "Graphic Design",
+    level: 90,
+  },
+  {
+    title: "3D Modeling",
     level: 60,
   },
   {
-    title: "Illustration ",
-    level: 100,
-  },
-  {
-    title: "Graphic Design ",
-    level: 100,
-  },
-  {
-    title: "Video Editing ",
-    level: 80,
+    title: "Web Development",
+    level: 40,
   },
 ];
 const languages = [
   {
-    title: "English",
+    title: "🇺🇸 English",
     level: 100,
   },
-]
+];
 
 const SkillsSection = () => {
   return (
@@ -217,46 +218,46 @@ const SkillsSection = () => {
 const ContactSection = () => {
   return (
     <Section>
-    <h2 className="text-5xl font-bold">Contact me</h2>
-    <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-      <form>
-        <label for="name" className="font-medium text-gray-900 block mb-1">
-          Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-        />
-        <label
-          for="email"
-          className="font-medium text-gray-900 block mb-1 mt-8"
-        >
-          Email
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-        />
-        <label
-          for="email"
-          className="font-medium text-gray-900 block mb-1 mt-8"
-        >
-          Message
-        </label>
-        <textarea
-          name="message"
-          id="message"
-          className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
-        />
-        <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
-          Submit
-        </button>
-      </form>
-    </div>
-  </Section>
-);
+      <h2 className="text-5xl font-bold">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
+        <form>
+          <label for="name" className="font-medium text-gray-900 block mb-1">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <label
+            for="email"
+            className="font-medium text-gray-900 block mb-1 mt-8"
+          >
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+          />
+          <button className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 ">
+            Submit
+          </button>
+        </form>
+      </div>
+    </Section>
+  );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,11 +2,11 @@ export const Menu = (props) => {
   const { onSectionChange, menuOpened, setMenuOpened } = props;
 
   return (
-  <>
+    <>
       <button
         onClick={() => setMenuOpened(!menuOpened)}
-        className="z-20 fixed top-12 right-12 p-3 bg-indigo-600 w-11 h-11 rounded-md" 
-        >
+        className="z-20 fixed top-12 right-12 p-3 bg-indigo-600 w-11 h-11 rounded-md"
+      >
         <div
           className={`bg-white h-0.5 rounded-md w-full transition-all ${
             menuOpened ? "rotate-45  translate-y-0.5" : ""
@@ -27,12 +27,12 @@ export const Menu = (props) => {
         className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col
       ${menuOpened ? "w-80" : "w-0"}`}
       >
-         <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
-         <MenuButton label="About" onClick={() => onSectionChange(0)} />
+        <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
+          <MenuButton label="About" onClick={() => onSectionChange(0)} />
           <MenuButton label="Skills" onClick={() => onSectionChange(1)} />
           <MenuButton label="Projects" onClick={() => onSectionChange(2)} />
           <MenuButton label="Contact" onClick={() => onSectionChange(3)} />
-         </div>
+        </div>
       </div>
     </>
   );
